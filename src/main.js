59eff85831ef06e22148b7274b78ec57265081e2
ddb324 <jiangdaoran@163.com>
2020-12-26 12:36:56 +0800

    创建第一个模块

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
+import './app1.css'
 import "./reset.css";
-import "./global.css";
-import $ from 'jquery';
-import "./app1.js";
-import "./app2.js";
-import "./app3.js";
-import "./app4.js";
+import $ from 'jquery'
+
+console.log($);
