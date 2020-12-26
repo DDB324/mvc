20af3d821aca3c2fe9ff744cf1e2039c0044122c
ddb324 <jiangdaoran@163.com>
2020-12-26 16:59:36 +0800

    完成app3的内容

--- src/app3.js
+++ src/app3.js
@@ -1,7 +1,8 @@
-import $ from 'jquery'
 import './app3.css'
+import $ from 'jquery'
+
 const $square = $('#app3 .square')
 
-$square.on('click', ()=>{
-  $square.toggleClass('active')
+$square.on('click', () => {
+    $square.toggleClass('active')
 })
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+import './global.css'
 import "./reset.css";
+
 import './app1.js'
 import './app2.js'
-import './global.css'
+import './app3.js'
