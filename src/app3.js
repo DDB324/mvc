f3b5aee0b46ebd281e09517166712acc09e8dac0
ddb324 <jiangdaoran@163.com>
2020-12-27 14:03:38 +0800

    点击后改变位置的数据保存

--- src/app3.js
+++ src/app3.js
@@ -1,8 +1,23 @@
-import './app3.css'
 import $ from 'jquery'
+import './app3.css'
 
 const $square = $('#app3 .square')
+const localKey = 'app3.position'
+const position = localStorage.getItem(localKey) === 'yes'
+
+// if (position) {
+//     $square.addClass('active')
+// } else {
+//     $square.removeClass('active')
+// }
+$square.toggleClass('active', position)
 
 $square.on('click', () => {
-    $square.toggleClass('active')
+    if ($square.hasClass('active')) {
+        $square.removeClass('active')
+        localStorage.setItem(localKey, 'no')
+    } else {
+        $square.addClass('active')
+        localStorage.setItem(localKey, 'yes')
+    }
 })
