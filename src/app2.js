599812209cdee2bbdf01345fe3dc46ed63353f48
ddb324 <jiangdaoran@163.com>
2020-12-27 13:33:41 +0800

    Tab切换的数据保存

--- src/app2.js
+++ src/app2.js
@@ -3,10 +3,13 @@ import $ from "jquery";
 
 const $tabBar = $('#app2  .tab-bar')
 const $tabContent = $('#app2  .tab-content')
+const localKey = 'app2.index'
+const index = localStorage.getItem(localKey) || 0
 
 $tabBar.on('click', 'li', (e) => {
     const $li = $(e.currentTarget)
     const index = $li.index()
+    localStorage.setItem(localKey, index)
     $li
         .addClass('selected')
         .siblings().removeClass('selected')
@@ -14,4 +17,4 @@ $tabBar.on('click', 'li', (e) => {
         .children().eq(index).addClass('active')
         .siblings().removeClass('active')
 })
-$tabBar.children().eq(0).trigger('click')
+$tabBar.children().eq(index).trigger('click')
