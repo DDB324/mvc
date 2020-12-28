7e2e689c4f74cbad55c611a11b54321d9f13c770
ddb324 <jiangdaoran@163.com>
2020-12-28 16:23:46 +0800

    优化v.el和v.container

--- src/app1.js
+++ src/app1.js
@@ -10,7 +10,6 @@ const m = {
 //视图相关放到v
 const v = {
     el: null,
-    container:null,
     html: ` <div>
         <div class="output">
             <span id="number">{{n}}</span>
@@ -23,23 +22,19 @@ const v = {
         </div>
     </div>`,
     render() {
-        const element = $(v.html.replace('{{n}}', m.data.n.toString()))
-        if (!v.el) {
-            v.el = element.appendTo(v.container)
-        } else {
-            v.el.replaceWith(element)
-            v.el = element
-        }
+        if (v.el.children.length !== 0) v.el.empty()//移除所有子节点
+        $(v.html.replace('{{n}}', m.data.n.toString()))
+            .appendTo(v.el)
     },
-    init(container){
-        v.container = $(container)
+    init(el) {
+        v.el = $(el)
         v.render()
     }
 }
 //其他内容放到c
 const c = {
-    init(container) {
-        v.init(container)
+    init(el) {
+        v.init(el)
         c.ui = {
             number: $('#number'),
             action: $('.actions')
@@ -47,7 +42,7 @@ const c = {
         c.bindEvents()
     },
     bindEvents() {
-        v.container.on('click', 'button', (e) => {
+        v.el.on('click', 'button', (e) => {
             const method = e.currentTarget.innerText
             if (method === '+1') {
                 m.data.n += 1
