2becd81c747028c6a5caca1dc94ed727b878bf2c
ddb324 <jiangdaoran@163.com>
2020-12-30 00:27:15 +0800

    修改app1.js的V对象

--- src/app1.js
+++ src/app1.js
@@ -1,6 +1,7 @@
 import "./app1.css";
 import $ from "jquery";
 import Model from './base/Model.js'
+import View from "./base/View";
 //eventBus
 const eventBus = $({})
 //数据相关放到m
@@ -8,47 +9,50 @@ const m = new Model({
     data: {
         n: parseInt(localStorage.getItem('number')) || 100
     },
-    update(data){
+    update(data) {
         Object.assign(m.data, data)
         eventBus.trigger('update:m')
         localStorage.setItem('number', m.data.n.toString())
     }
 })
 //视图相关放到v
-const v = {
-    el: null,
-    html: ` <div>
-        <div class="output">
-            <span id="number">{{n}}</span>
-        </div>
-        <div class="actions">
-            <button id="add1">+1</button>
-            <button id="minus1">-1</button>
-            <button id="mul2">*2</button>
-            <button id="divide2">÷2</button>
-        </div>
-    </div>`,
-    render(data) {
-        if (v.el.children.length !== 0) v.el.empty()//移除所有子节点
-        $(v.html.replace('{{n}}', data))
-            .appendTo(v.el)
-    },
-    init(container) {
-        v.el = $(container)
-    }
-}
 //其他内容放到c
 const c = {
+    container: null,
+    v: null,
+    initV() {
+        c.v = new View({
+            el: c.container,
+            html: ` 
+                <div>
+                    <div class="output">
+                        <span id="number">{{n}}</span>
+                    </div>
+                    <div class="actions">
+                        <button id="add1">+1</button>
+                        <button id="minus1">-1</button>
+                        <button id="mul2">*2</button>
+                        <button id="divide2">÷2</button>
+                    </div>
+                </div>`,
+            render(data) {
+                if (c.v.el.children.length !== 0) c.v.el.empty()//移除所有子节点
+                $(c.v.html.replace('{{n}}', data))
+                    .appendTo(c.v.el)
+            },
+        })
+    },
     init(container) {
-        v.init(container)
-        v.render(m.data.n)
+        c.container = container
+        c.initV()
+        c.v.render(m.data.n)
         c.ui = {
             number: $('#number'),
             action: $('.actions')
         }
         c.autoBindEvents()
         eventBus.on('update:m', () => {
-            v.render(m.data.n)
+            c.v.render(m.data.n)
         })
     },
     events: {
@@ -73,7 +77,7 @@ const c = {
         for (let key in c.events) {
             const [part1, part2] = key.split(' ')
             const method = c[c.events[key]]
-            v.el.on(part1, part2, method)
+            c.v.el.on(part1, part2, method)
         }
     }
 }
