ffa2b801769aba595eef4fdee96600a7670ceba3
ddb324 <jiangdaoran@163.com>
2020-12-29 14:14:14 +0800

    将app2.js内容封装为MVC三个对象

--- src/app2.js
+++ src/app2.js
@@ -1,30 +1,65 @@
 import "./app2.css";
 import $ from "jquery";
-const html = ` <section id="app2">
+// eventBus
+const eventBus = $({})
+//数据相关放到m
+const m = {
+    data: {
+        index: parseInt(localStorage.getItem('app2.index')) || 0
+    },
+    update(data) {
+        Object.assign(m.data, data)
+        eventBus.trigger('update:m')
+        localStorage.setItem('app2.index', m.data.index.toString())
+    }
+}
+//视图相关放到v
+const v = {
+    el: null,
+    html: (index) => {
+        return ` <div id="app2">
         <ol class="tab-bar">
-            <li><span>导航1</span></li>
-            <li><span>导航2</span></li>
-        </ol>
-        <ol class="tab-content">
-            <li>内容1</li>
-            <li>内容2</li>
-        </ol>
-    </section>`
-const $element = $(html).appendTo('body>.page')
-const $tabBar = $('#app2  .tab-bar')
-const $tabContent = $('#app2  .tab-content')
-const localKey = 'app2.index'
-const index = localStorage.getItem(localKey) || 0
+             <li class="${index === 0 ? 'selected' : ''}" data-index="0"><span>导航1</span></li>
+             <li class="${index === 1 ? 'selected' : ''}" data-index="1"><span>导航2</span></li>
+         </ol>
+         <ol class="tab-content">
+             <li class="${index === 0 ? 'active' : ''}">内容1</li>
+             <li class="${index === 1 ? 'active' : ''}">内容2</li>
+         </ol>
+     </div>`
+    },
+    render(index) {
+        if (v.el.children.length !== 0) v.el.empty()//移除所有子节点
+        $(v.html(index)).appendTo(v.el)
+    },
+    init(container) {
+        v.el = $(container)
+    }
+}
+//其他内容放到c
+const c = {
+    init(container) {
+        v.init(container)
+        v.render(m.data.index)
+        c.autoBindEvents()
+        eventBus.on('update:m', () => {
+            v.render(m.data.index)
+        })
+    },
+    events: {
+        'click .tab-bar>li': 'action',
+    },
+    action(e) {
+        const index = parseInt($(e.currentTarget).data('index'));
+        m.update({index: index})
+    },
+    autoBindEvents() {
+        for (let key in c.events) {
+            const [part1, part2] = key.split(' ')
+            const method = c[c.events[key]]
+            v.el.on(part1, part2, method)
+        }
+    }
+}
 
-$tabBar.on('click', 'li', (e) => {
-    const $li = $(e.currentTarget)
-    const index = $li.index()
-    localStorage.setItem(localKey, index)
-    $li
-        .addClass('selected')
-        .siblings().removeClass('selected')
-    $tabContent
-        .children().eq(index).addClass('active')
-        .siblings().removeClass('active')
-})
-$tabBar.children().eq(index).trigger('click')
+export default c
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import './global.css'
 import "./reset.css";
 
-import c from './app1.js'
-import './app2.js'
+import app1 from './app1.js'
+import app2 from './app2.js'
 import './app3.js'
 import './app4.js'
 
-c.init('#app1')
+app1.init('#app1')
+app2.init('#app2')
