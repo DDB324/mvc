b97c0d7f63e36b05d43e817f180462866ffd7c7b
ddb324 <jiangdaoran@163.com>
2020-12-30 16:10:32 +0800

    将view对象中共用的属性放到view类中

--- src/base/View.js
+++ src/base/View.js
@@ -1,10 +1,24 @@
 import $ from 'jquery'
 
 class View {
-    constructor({el, html, render}) {
-        this.el = $(el)
-        this.html = html
-        this.render = render
+    // constructor({el, html, render,events,data,eventBus,method}) {
+    constructor(options) {
+        Object.assign(this,options)
+    }
+    init() {
+        this.el = $(this.el)
+        this.render(this.data)
+        this.autoBindEvents()
+        this.eventBus.on('update:m', () => {
+            this.render(this.data)
+        })
+    }
+    autoBindEvents() {
+        for (let key in this.events) {
+                const [part1, part2] = key.split(' ')
+                const method = this[this.events[key]]
+                this.el.on(part1, part2, method)
+           }
     }
 }
 
